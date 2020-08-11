aaf0088a5f7555f0ca72d575c9db58cc2f926ac6
miakauppila <mia.kauppila@gmail.com>
2020-08-11 23:52:23 +0300

    tehtävä 4.2

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
+const app = require('./app') // import Express-app
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = config.MONGODB_URI
-mongoose
-    .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-    response.send('<h2>Please check the route parameters.</h2>')
-})
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result) //201 Created
-        })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
