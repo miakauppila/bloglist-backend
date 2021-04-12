8a1778ae50467be147757c241bbe2b680b8ba5e2
miakauppila <mia.kauppila@gmail.com>
2021-04-12 23:48:19 +0300

    refactor blogsRouter and eliminate try-catch (express-async-errors)

--- controllers/blogsRouter.js
+++ controllers/blogsRouter.js
@@ -10,89 +10,63 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.get('/:id', async (request, response, next) => {
-  try {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-      response.json(blog)
-    }
-    else {
-      response.status(404).end()
-    }
-  }
-  catch(error){
-    next(error)
-  }
-})
-
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const token = request.token
   if (!token) {
     return response.status(401).json({ error: 'token missing' })
   }
-  try {
-    //decoded token includes username & id
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: 'invalid token' })
-    }
-    //search for the correct user
-    const user = await User.findById(decodedToken.id)
-
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes === undefined ? 0 : body.likes,
-      user: user._id
-    })
-
-    const savedBlog = await blog.save()
-    //add savedBlog to user's blogs array
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    response.status(201).json(savedBlog)
-
+  //decoded token includes username & id
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
   }
-  catch (error) {
-    next(error)
-  }
+  //search for the correct user
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : body.likes,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  //add savedBlog to user's blogs array
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
-  const token = request.token
-  if (!token) {
+blogsRouter.delete('/:id', async (request, response) => {
+  if (!request.token) {
     return response.status(401).json({ error: 'token missing' })
   }
-  try {
-    //decoded token includes username & id
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return response.status(401).json({ error: 'invalid token' })
-    }
-    //search for the correct user & blog
-    const user = await User.findById(decodedToken.id)
-    const blog = await Blog.findById(request.params.id)
-    if(!blog) {
-      return response.status(401).json({ error: 'blog does not exist' })
-    }
-    // blog.user is ObjectId so both must be converted to string
-    else if (blog.user.toString() === user._id.toString()) {
-      await Blog.deleteOne(blog)
-      console.log('deleted blog')
-      response.status(204).end() // no content returned
-    }
-    else {
-      return response.status(401).json({ error: 'token not valid for this operation' })
-    }
+  //decoded token includes username & id
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
   }
-  catch (error) {
-    next(error)
+  //search for the correct user & blog
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(401).json({ error: 'blog does not exist' })
+  }
+  // blog.user is ObjectId so both must be converted to string
+  else if (blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'only the creator can delete blogs' })
+  }
+  else {
+    await Blog.deleteOne(blog)
+    user.blogs = user.blogs.filter(blog => blog.id.toString() !== request.params.id.toString())
+    await user.save()
+    response.status(204).end() // no content returned
   }
 })
 
-blogsRouter.put('/:id', async (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const changedBlog = {
@@ -102,13 +76,11 @@ blogsRouter.put('/:id', async (request, response, next) => {
     likes: body.likes,
   }
 
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, changedBlog,
-      { new: true, omitUndefined: true }) //options: undefined fields will not be sent
-    response.status(200).json(updatedBlog)
-  } catch (error) {
-    next(error)
-  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, changedBlog,
+    { new: true, omitUndefined: true })
+  //mongoose options: new: true returns the document after update
+  // omitUndefined: undefined fields in the request deleted and not sent
+  response.status(200).json(updatedBlog)
 })
 
 module.exports = blogsRouter
